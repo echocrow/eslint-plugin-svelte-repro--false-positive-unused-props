3bce2aece8f457dde7312578620aab5767e0a396
echocrow <git@stackblitz.com>
2025-11-13 15:27:16 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# eslint-plugin-svelte-repro--false-positive-unused-props
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/echocrow/eslint-plugin-svelte-repro--false-positive-unused-props)
---
+++ src/App.svelte
@@ -0,0 +1,26 @@
+<script lang="ts" module>
+export class FooController<T extends string = string> {
+  #foo: T
+
+  constructor(foo: T) {
+    this.#foo = foo
+  }
+
+  onclose() {
+    console.info(this.#foo)
+  }
+}
+</script>
+
+<script lang="ts">
+interface Props {
+  ctrl: FooController
+}
+let {ctrl}: Props = $props()
+
+function onclose() {
+  ctrl.onclose()
+}
+</script>
+
+<dialog {onclose}>Foo</dialog>
